c0678f03e1311ab146f1bf39651b8ae98ef1d936
Princelad <ladprince1112@gmail.com>
2024-12-27 20:47:08 +0530

    Generated board and pieces

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -23,8 +23,40 @@ def main():
     clock = pg.time.Clock()
 
     screen.fill(pg.Color("white"))
-    gs = ChessEngine.GameState()
+    game_state = ChessEngine.GameState()
 
+    load_images()
 
+    running = True
 
-main()
+    while running:
+        for event in pg.event.get():
+            if event.type == pg.QUIT:
+                running = False
+
+        draw_game_state(screen, game_state)
+        clock.tick(MAX_FPS)
+        pg.display.flip()
+
+def draw_game_state(screen, game_state):
+    draw_board(screen)
+    draw_pieces(screen, game_state.board)
+
+def draw_board(screen):
+    colors = [pg.Color("white"), pg.Color("gray")]
+
+    for rows in range(DIMENSION):
+        for cols in range(DIMENSION):
+            color = colors[(rows + cols) % 2]
+            pg.draw.rect(screen, color, pg.Rect(cols * SQ_SIZE, rows * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+def draw_pieces(screen, board):
+    for rows in range(DIMENSION):
+        for cols in range(DIMENSION):
+            piece = board[rows][cols]
+
+            if piece != "--" :
+                screen.blit(IMAGES[piece], pg.Rect(cols * SQ_SIZE, rows * SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+if __name__ == "__main__":
+    main()
