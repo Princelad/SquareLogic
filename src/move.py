86dd943c083fc5cec390aa9b1a91cd62e74a421a
Princelad <ladprince1112@gmail.com>
2025-02-07 20:25:22 +0530

    fixed color bug

--- src/move.py
+++ src/move.py
@@ -2,9 +2,11 @@ class Move:
     """
     Represents a chess move with start and end positions, and metadata.
     """
-    ranks_to_rows = {"1": 7, "2": 6, "3": 5, "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
+    ranks_to_rows = {"1": 7, "2": 6, "3": 5,
+                     "4": 4, "5": 3, "6": 2, "7": 1, "8": 0}
     rows_to_ranks = {v: k for k, v in ranks_to_rows.items()}
-    files_to_cols = {"a": 0, "b": 1, "c": 2, "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
+    files_to_cols = {"a": 0, "b": 1, "c": 2,
+                     "d": 3, "e": 4, "f": 5, "g": 6, "h": 7}
     cols_to_files = {v: k for k, v in files_to_cols.items()}
 
     def __init__(self, start_sq, end_sq, board, is_enpassant=False, is_castle=False):
@@ -16,17 +18,18 @@ class Move:
         self.piece_captured = board[self.end_row][self.end_col]
         # Pawn Promotion
         self.is_pawn_promotion = ((self.piece_moved == "wP" and self.end_row == 0) or (
-                self.piece_moved == "bP" and self.end_row == 7))
+            self.piece_moved == "bP" and self.end_row == 7))
 
         # Enpassant
         self.is_enpassant = is_enpassant
         if is_enpassant:
-            self.piece_captured = "wP" if self.piece_moved == "wP" else "bP"
+            self.piece_captured = "bP" if self.piece_moved == "wP" else "wP"
 
         # Castle move
         self.is_castle = is_castle
 
-        self.move_id = self.start_row * 1000 + self.start_col * 100 + self.end_row * 10 + self.end_col
+        self.move_id = self.start_row * 1000 + self.start_col * \
+            100 + self.end_row * 10 + self.end_col
 
     def __eq__(self, other):
         return isinstance(other, Move) and self.move_id == other.move_id
